27231db7710e1fa89d81173a2650926103e1ab29
Austin-Karren <austin.t.karren@gmail.com>
2021-01-14 14:40:23 -0700

    add

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+require('dotenv').config();
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
 const express = require('express'),
-      port = 3333,
+      massive = require('massive'),
+      authCrtl = require('./controllers/authController'),
+      session = require('express-session'),
+      port = SERVER_PORT,
       app = express();
 
 app.use(express.json());
 
-app.listen(port, () => console.log(`Server running on ${port}`));
+app.use(session({
+   resave: false,
+   saveUninitialized: true,
+   secret: SESSION_SECRET,
+   cookie: {maxAge: 1000 * 60 * 60 * 24}
+}))
+
+massive({
+   connectionString: CONNECTION_STRING,
+   ssl: {rejectUnauthorized: false}
+}).then(db => {
+   app.listen(port, () => console.log(`Server running on ${port}`));
+   app.set('db', db);
+   console.log('db connected')
+})
+
+app.post('/auth/register', authCrtl.register);
+app.post('/auth/login', authCrtl.login);
+app.get('/auth/logout', authCrtl.logout);
